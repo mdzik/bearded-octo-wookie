821ff22413d9db15d0c49ee23d6e22783bf626a7
mdzik <dziki01@gmail.com>
2016-06-24 10:43:50 +0200

    way better than before ;)

--- lbm.py
+++ lbm.py
@@ -31,7 +31,52 @@ def forAll9do(f, *args):
     for i in range(9):
         f(i, *args)
 
+def init(_i, _W, _e, _rho, _U, _fin):
+    _fin[:,:,_i] =  _W[_i] * _rho[:,:]
+    cu  = 3. * ( _U[:,:,0] * _e[_i,0] + _U[:,:,1] * _e[_i,1])
+    _fin[:,:,_i] = _W[_i] * _rho[:,:] * (1. + cu[:,:] + 0.5*cu[:,:]*cu[:,:] - (3./2.) * ( _U[:,:,0]**2 + _U[:,:,1]**2 ) )
 
-#def f(i, a,b) : 
-#    print a,b        
-#forAll9do(f, 'a', 1)
+
+def stream(_i, _ox, _oy, _fin, _fou):
+    _fin[:,:,_i] = _fou[_ox[_i],_oy[_i],_i] 
+    
+def BGK(_i, _U, _rho, _e, _W, _tau, _F, _fin, _fou):
+
+    cu  = 3. * ( _U[:,:,0] * _e[_i,0] + _U[:,:,1] * _e[_i,1])
+    feq1 = _W[_i] * _rho[:,:] * (1. + cu[:,:] + 0.5*cu[:,:]*cu[:,:] - (3./2.) * ( _U[:,:,0]**2 + _U[:,:,1]**2 ) )
+
+    cu  = 3. * ( (_U[:,:,0]+_F[:,:,0]) * _e[_i,0] + (_U[:,:,1] + _F[:,:,1]) * _e[_i,1])    
+    feq2 = _W[_i] * _rho[:,:] * (1. + cu[:,:] + 0.5*cu[:,:]*cu[:,:] - (3./2.) * ( (_U[:,:,0]+_F[:,:,0])**2 + (_U[:,:,1]+_F[:,:,1])**2 ) )
+
+    _fou[:,:,_i] = _fin[:,:,_i] + (_tau) * ( feq1[:,:] - _fin[:,:,_i] ) + (feq2[:,:]-feq1[:,:])
+
+
+def BB(_i, _bx, _by, _e_opp, _fin, _fou):
+    for bx,by in zip(_bx,_by):  
+        _fou[bx,by,_i] = _fin[bx,by,_e_opp[_i]]     
+
+
+
+def MRT_meq(_i, _U, _rho, _fmeq, _meq):
+    _meq[_i] = _fmeq[i](_rho, _U[:,:,0], _U[:,:,1])
+
+def applyBC(A, f):
+    shape = f.shape
+    _f = np.ones((shape[0]+1, shape[1]+1))
+    _f[:,:-1] = f
+    return A.dot(_f.T).T
+
+def applyCornerBC(A, f):
+    shape = f.shape
+    _f = np.ones(shape[0]+1)
+    _f[:-1] = f
+    return A.dot(_f.T).T
+
+def applyWideBC(A, f):
+    shape = f.shape
+    _f = np.ones((shape[0], shape[1]+1))
+    _f[:,:-1] = f
+    assert len(A[:,0,0] == len(_f[:,0]))
+    for i, ff in enumerate(_f):
+        f[i,:] = A[i,:,:].dot(_f[i,:]).T
+    return f
--- simple.py
+++ simple.py
@@ -12,6 +12,12 @@ font = {
 
 mpl.rc('font', **font)
 
+legend = {
+    'fontsize' : 'medium'
+}
+
+mpl.rc('legend', **legend)
+
 def gimmeFigureA4(ratioY=0.5, ratioX=0.7, **kwargs):
     print (ratioX*8.27, ratioY*11.7)
     tight_layout = True
@@ -20,7 +26,22 @@ def gimmeFigureA4(ratioY=0.5, ratioX=0.7, **kwargs):
     print "tight =", tight_layout
     if kwargs.has_key('fig'):
             kwargs['fig'].set_size_inches((ratioX*8.27, ratioY*11.7))
+            return kwargs['fig']
     else:           
-        plt.figure(figsize=(ratioX*8.27, ratioY*11.7),tight_layout=tight_layout)
+        return plt.figure(figsize=(ratioX*8.27, ratioY*11.7),tight_layout=tight_layout)
 def gimmeFigureA4LinePlot(**kwargs):
-    gimmeFigureA4(0.25,**kwargs)
+    return gimmeFigureA4(0.25,**kwargs)
+   
+def gimmeAxisA4LinePlotLegendBottom():
+    fig = gimmeFigureA4LinePlot(tight_layout=False)
+    return fig.add_subplot(111)    
+ 
+    
+def wrapAxisLegendBottomA4(ax):
+    # Shrink current axis by 20%
+    
+    box = ax.get_position()
+    ax.set_position([box.x0 , box.y0 * 3, box.width , box.height * 0.8])
+    
+    # Put a legend to the right of the current axis
+    ax.legend(loc='lower left', bbox_to_anchor=(0.0, -0.5))
